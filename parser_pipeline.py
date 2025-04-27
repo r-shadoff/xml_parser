93227fadf4c7427f2ea59b9230990465eb93c64d
“Rachel <rachelshadoff@hotmail.ca>
2025-04-27 11:25:29 -0400

    pipeline tested on directory 00/00

---
+++ parser_pipeline.py
@@ -0,0 +1,24 @@
+# Import Necessary Libraries
+import os
+import functions as f
+
+download_path = "/Users/rachel/Desktop/xml_parser_test"
+
+# Step 1. Download folder contents
+#f.download_pmc("00", "00", download_path)
+
+# Step 2. Uncompress archives 
+#f.uncompress_tar(download_path)
+
+# Step 3. Sort viable records
+# f.sort_data(download_path)
+
+# # Step 4. Parse Sorted Records
+# f.grab_figure_data(download_path)
+# f.clean_text(download_path)
+
+# # Step 5. Clean up
+f.file_shuttle(download_path)
+f.remove_file_type(download_path, extensions=(".gif", ".doc", ".docx", ".html", ".mov"))
+f.unique_exts(download_path) 
+f.no_trace(download_path)
