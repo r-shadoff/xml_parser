a9616b86314fc44815298c7877b44439c1166088
“Rachel <rachelshadoff@hotmail.ca>
2025-04-27 11:26:05 -0400

    uncommented steps 2-4

--- parser_pipeline.py
+++ parser_pipeline.py
@@ -5,17 +5,17 @@ import functions as f
 download_path = "/Users/rachel/Desktop/xml_parser_test"
 
 # Step 1. Download folder contents
-#f.download_pmc("00", "00", download_path)
+f.download_pmc("00", "00", download_path)
 
 # Step 2. Uncompress archives 
-#f.uncompress_tar(download_path)
+f.uncompress_tar(download_path)
 
 # Step 3. Sort viable records
-# f.sort_data(download_path)
+f.sort_data(download_path)
 
 # # Step 4. Parse Sorted Records
-# f.grab_figure_data(download_path)
-# f.clean_text(download_path)
+f.grab_figure_data(download_path)
+f.clean_text(download_path)
 
 # # Step 5. Clean up
 f.file_shuttle(download_path)
