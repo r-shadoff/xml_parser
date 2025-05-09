2a17f8d88a6487df6a724c29bdc0b3f49b42f4b3
“Rachel <rachelshadoff@hotmail.ca>
2025-05-09 13:39:54 -0400

    testing combine_dataframes step

--- parser_pipeline.py
+++ parser_pipeline.py
@@ -6,17 +6,17 @@ import re
 download_path = "/Users/rachel/Desktop/xml_parser_test"
 
 # Step 1. Download folder contents
-f.download_pmc("00", "00", download_path)
+#f.download_pmc("00", "00", download_path)
 
 # Step 2. Uncompress archives 
-f.uncompress_tar(download_path)
+#f.uncompress_tar(download_path)
 
 # Step 3. Sort viable records
-f.sort_data(download_path)
+#f.sort_data(download_path)
 
 # # Step 4. Parse Sorted Records
-f.grab_figure_data(download_path)
-f.grab_spacy_text(download_path)
+#f.grab_figure_data(download_path)
+#f.grab_spacy_text(download_path)
 f.combine_dataframes(download_path)
 #TO DO: Add spacy text duplicate clean up in the clean_text function -> f.clean_text(download_path)
 
