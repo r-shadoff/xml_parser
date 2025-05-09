924c8f40214cc41e09b8517d237e00349329328f
“Rachel <rachelshadoff@hotmail.ca>
2025-05-09 13:37:41 -0400

    Restored spacy text extraction steps for push to new branch

--- parser_pipeline.py
+++ parser_pipeline.py
@@ -1,6 +1,7 @@
 # Import Necessary Libraries
 import os
 import functions as f
+import re
 
 download_path = "/Users/rachel/Desktop/xml_parser_test"
 
@@ -13,12 +14,14 @@ f.uncompress_tar(download_path)
 # Step 3. Sort viable records
 f.sort_data(download_path)
 
-# Step 4. Parse Sorted Records
+# # Step 4. Parse Sorted Records
 f.grab_figure_data(download_path)
-f.clean_text(download_path)
+f.grab_spacy_text(download_path)
+f.combine_dataframes(download_path)
+#TO DO: Add spacy text duplicate clean up in the clean_text function -> f.clean_text(download_path)
 
 # # Step 5. Clean up
-f.file_shuttle(download_path)
-f.remove_file_type(download_path, extensions=(".gif", ".doc", ".docx", ".html", ".mov"))
-f.unique_exts(download_path) 
-f.no_trace(download_path)
+# f.file_shuttle(download_path)
+# f.remove_file_type(download_path, extensions=(".gif", ".doc", ".docx", ".html", ".mov"))
+# f.unique_exts(download_path) 
+# f.no_trace(download_path)
